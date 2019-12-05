d9d8730ae1aa09b737af10eb158f169dc35184ed
Tara Sherman <taradelin@gmail.com>
2019-12-04 19:17:53 -0500

    set up initial requirements, server run

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+
+// library
+const express = require('express');
+
+// database file
+const users = require('./data/db.js');
+
+// global object
+const server = express();
+
+// middleware
+server.use(express.json());
+
+
+
+// listen on port 3000
+server.listen(3000, () => {
+  console.log('Server is running on port 3000.');
+});
