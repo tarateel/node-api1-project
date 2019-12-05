462b019b26cf8e37c62bf8b58d324f6633d7eb11
Tara Sherman <taradelin@gmail.com>
2019-12-04 19:19:59 -0500

    Created and tested endpoint to add a user

--- index.js
+++ index.js
@@ -12,6 +12,33 @@ const server = express();
 // middleware
 server.use(express.json());
 
+// add user to db
+server.post('/api/users', (req, res) => {
+  //define name and bio as request body
+  const { name, bio } = req.body;
+  // if either name or bio are missing
+  if (!name || !bio) {
+    // respond with 'bad request' status code and JSON message
+    res.status(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    })
+    //otherwise
+  } else {
+    // insert user object
+    users.insert(req.body)
+    .then(user => {
+      // and respond with 'status created' code and the id of the inserted user object
+      res.status(201).json(user.id)
+    })
+    .catch(err => {
+      // if there is an error, respond with 'internal server error' code and a JSON message
+      res.status(500).json({
+        errorMessage: "There was an error while saving the user to the database."
+      })
+    });
+  };
+});
+
 
 
 // listen on port 3000
